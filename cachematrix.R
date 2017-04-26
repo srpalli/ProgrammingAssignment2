03b514eda8200c614785e434162a7010726a2164
Srinivas <srpalli@yahoo.com>
2017-04-26 20:41:54 +0100

    coursera-rprog-assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  setmatrix <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  getmatrix <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(setmatrix=setmatrix, getmatrix=getmatrix, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
 
+
+# This function computes the inverse of the special matrix returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), then cacheSolve 
+# should retrieve the inverse from the cache.
+
+
+
+# This function assumes that the matrix is always invertible.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
+## Sample run:
+
+# my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+#my_matrix$getmatrix()
+##[,1] [,2]
+##[1,]    1    3
+##[2,]    2    4
+
+# my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+#  my_matrix$getinverse()
+##NULL
+
+#cacheSolve(my_matrix)
+### [,1] [,2]
+### [1,]   -2  1.5
+### [2,]    1 -0.5
+# cacheSolve(my_matrix)
+### getting cached data
+### [,1] [,2]
+### [1,]   -2  1.5
+### [2,]    1 -0.5
+# my_matrix$getInverse()
+### [,1] [,2]
+### [1,]   -2  1.5
+### [2,]    1 -0.5
+# my_matrix$setmatrix(matrix(c(2, 2, 1, 4), 2, 2))
+# my_matrix$getmatrix()
+### [,1] [,2]
+### [1,]    2    1
+### [2,]    2    4
+# my_matrix$getinverse()
+### NULL
